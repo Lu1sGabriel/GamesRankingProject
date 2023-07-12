a4f827086aaf53bb5989a319385e122197e7fd85
Luis Gabriel Goés <luisgabriel533@gmail.com>
2023-07-12 11:25:31 -0300

    Redoing the change to Page. List has been put back

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -3,14 +3,12 @@ package com.devsuperior.dslist.controllers;
 import com.devsuperior.dslist.dtos.GameMinDto;
 import com.devsuperior.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -20,9 +18,8 @@ public class GameController {
     private GameService gameService;
 
     @GetMapping
-    public ResponseEntity<Page<GameMinDto>> findAll(@PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
-        Page<GameMinDto> games = gameService.findAll(pageable);
-        return ResponseEntity.ok(games);
+    public ResponseEntity<List<GameMinDto>> findAll() {
+        return ResponseEntity.ok(gameService.findAll());
     }
 
 }
--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -4,17 +4,18 @@ import com.devsuperior.dslist.dtos.GameMinDto;
 import com.devsuperior.dslist.models.GameModel;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
-    public Page<GameMinDto> findAll(Pageable pageable) {
-        Page<GameModel> result = gameRepository.findAll(pageable);
-        return result.map(GameMinDto::new);
+    public List<GameMinDto> findAll() {
+        List<GameModel> result = gameRepository.findAll();
+        return result.stream().map(GameMinDto::new).toList();
+
     }
 }
